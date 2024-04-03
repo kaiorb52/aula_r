2ab889aa353a3ce9d5f04e0adfa683c519c421f8
Kaio R. Barbara <kaio.rb@hotmail.com>
2024-04-03 11:45:50 -0300

    -

--- 1. Scripts/p_1/script.R
+++ 1. Scripts/p_1/script.R
@@ -11,8 +11,8 @@ url <- "https://www.ispdados.rj.gov.br/Arquivos/BaseDPEvolucaoMensalCisp.csv"
 
 isp_rj <- fread(url, encoding = "Latin-1")
 
-# 3. ANALISE ----
-## 3.1 Questão I, Qual é o ano com mais homicídios dolosos? ----
+# 2. ANALISE ----
+## 2.1 Questão I, Qual é o ano com mais homicídios dolosos? ----
 
 q_1 <- isp_rj |>
   group_by(ano) |>
@@ -20,9 +20,9 @@ q_1 <- isp_rj |>
   arrange(desc(sum_hom_doloso)) |>
   head(5) 
 
-print(q_1) # Segundo os dados do isp, o ano com mais mortes por homisidio doloso é 2003.
+print(q_1) # Segundo os dados do isp, o ano com mais mortes por homisidio doloso é 2002.
 
-## 3.2 Questão II, Em qual município, considerando todo o intervalo entre 2012 e 2018, há maior proporção de autos de resistência em relação ao total de casos de letalidade violenta (obs.: é o período todo entre 2012 e 2018, não ano-a-ano)? ----
+## 2.2 Questão II, Em qual município, considerando todo o intervalo entre 2012 e 2018, há maior proporção de autos de resistência em relação ao total de casos de letalidade violenta (obs.: é o período todo entre 2012 e 2018, não ano-a-ano)? ----
 
 q_2 <- isp_rj |>
   filter(ano >= 2012 & ano <= 2018) |>
@@ -38,7 +38,7 @@ q_2 <- isp_rj |>
 print(q_2)  # considerando o intervalo entre os anos de 2012 e 2018, 
 #o municipio de Niterói se demostra como o de maior homidio de intervenção policial propocional a quantidade de letalidade violenta
 
-## 3.3 Questão III, Qual é a delegacia/ano com mais casos de letalidade violenta por 100.000 habitantes? ----
+## 2.3 Questão III, Qual é a delegacia/ano com mais casos de letalidade violenta por 100.000 habitantes? ----
 
 q_3 <- isp_rj |>
   group_by(cisp, ano) |>
@@ -52,7 +52,7 @@ isp_rj %>%
   filter(cisp == 39) %>%
   select(munic) # Rio de Janeiro
 
-# Como demostrado, o 39º CISP da zona Oeste do Rio de Janeito,
+# Como demostrado, o 39º CISP da zona Oeste do Rio de Janeiro,
 #no ano de 2003 contem o maior numero de letalidade violotenta. 
 #No universo de 100.000 habitantes ocorre 1 morte por letalidade violetenta para cada 250 habitante
 
