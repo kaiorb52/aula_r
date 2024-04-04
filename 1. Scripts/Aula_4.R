b5c867b05922fc114631a84faca85a51038e3956
Kaio Ribeiro <kaio.rb@hotmail.com>
2024-04-04 17:22:16 -0300

    -

--- 1. Scripts/Aula_4.R
+++ 1. Scripts/Aula_4.R
@@ -1,9 +1,13 @@
 
-library(tidyverse)
-library(data.table)
+pacman::p_load(
+  tidyverse,
+  data.table,
+  here  
+)
 
 isp <- fread("0. Docs/BaseDPEvolucaoMensalCisp.csv", encoding = "Latin-1")
-pop <- fread("0. Docs/PopulacaoEvolucaoMensalCisp.csv", encoding = "Latin-1")
+pop <- read_delim("0. Docs/PopulacaoEvolucaoMensalCisp.csv", 
+                  delim = ";", escape_double = FALSE, trim_ws = TRUE)
 
 glimpse(isp)
 glimpse(pop)
@@ -11,3 +15,61 @@ glimpse(pop)
 pop$cisp <- pop$circ
 
 isp_pop <- left_join(isp, pop) %>% select(-circ)
+
+any(is.na(isp_pop$pop_circ))
+
+isp_pop <- left_join(isp, pop) %>% select(-circ)
+
+isp_na <- isp_pop %>% filter(is.na(pop_circ))
+
+table(isp_na$ano)
+table(isp$ano)
+table(pop$ano)
+
+isp_novo <- isp_pop %>% filter(between(ano, 2005, 2021)
+)
+
+nomes_dp <- fread("0. Docs/Relacao_RISPxAISPxCISP.csv", encoding = "Latin-1") %>% janitor::clean_names() %>% select(-risp, -aisp)
+
+isp_novo_0 <- left_join(isp_novo, nomes_dp)
+
+# taxa de letatidade violenta
+# hominicidio doloso 
+# autos de resistencias 
+# policial matar
+
+isp_novo_10 <- isp_novo_0 %>%
+  group_by(cisp) %>%
+    summarise(
+      mun = paste(municipio, collapse = "-"),
+      ter = paste(unidade_territorial, collapse = "-"),
+    )
+
+x <- c("Universidade Norte Fluminense Darcy Ribeiro", "UENF")
+x2 <- paste(x, collapse = "-")
+
+print(x2)
+isp_novo_0$pop_circ
+
+pop_ano <-  isp_novo_0 %>%
+  select(munic, ano, mes, pop_circ) %>%
+  filter(mes == 12) %>%
+  group_by(ano) %>%
+  summarise(sum_pop = sum(pop_circ))
+
+crimes <- isp_novo_0 %>%
+  group_by(ano) %>%
+  summarise(
+    Count = n(),
+    sum_ltd_violenta = sum(letalidade_violenta, na.rm = TRUE),
+    sum_autos_resis = sum(hom_por_interv_policial, na.rm = TRUE),
+    sum_hom_doloso = sum(hom_doloso, na.rm = TRUE))
+
+crimes_pop_ano <- left_join(crimes, pop_ano) %>%
+  mutate(perct_ltd_violenta = (sum_ltd_violenta/sum_pop)*100000,
+         perct_autos_resis = (sum_autos_resis/sum_pop)*100000,
+         perct_hom_doloso = (sum_hom_doloso/sum_pop)*100000,
+         ind_ltd_violenta = sum_autos_resis/sum_pop,
+  ) %>%
+  mutate(ind_ltd_violenta_pop = (ind_ltd_violenta/sum_ltd_violenta)*100000)
+
